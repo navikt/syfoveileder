dc8ff8666e0a46099d3d449fdd8dbc1189ff144e
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2019-05-02 10:50:40 +0200

    Tar i bruk OIDC-security

--- src/main/kotlin/no/nav/syfo/selftest/SelftestController.kt
+++ src/main/kotlin/no/nav/syfo/selftest/SelftestController.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.selftest
 
+import no.nav.security.spring.oidc.validation.api.Unprotected
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -15,10 +16,12 @@ class SelftestController {
     val isAlive: String
         @ResponseBody
         @RequestMapping(value = ["/isAlive"], produces = [MediaType.TEXT_PLAIN_VALUE])
+        @Unprotected
         get() = APPLICATION_LIVENESS
 
     val isReady: String
         @ResponseBody
         @RequestMapping(value = ["/isReady"], produces = [MediaType.TEXT_PLAIN_VALUE])
+        @Unprotected
         get() = APPLICATION_READY
 }
