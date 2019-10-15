70fcc9a55a247a90117e8529580d94613170d578
Kristian Storvoll <kristianstorvoll@gmail.com>
2019-10-15 10:50:35 +0200

    --

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.github.jengelman.gradle.plugins.shadow.transformers.PropertiesFileTransformer
 import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
@@ -22,6 +21,7 @@ buildscript {
     }
 }
 
+
 repositories {
     mavenCentral()
     jcenter()
@@ -33,32 +33,32 @@ repositories {
 }
 
 dependencies {
-    compile ("org.apache.cxf:cxf-spring-boot-starter-jaxws:3.3.3")
-    compile ("org.apache.cxf:cxf-rt-features-logging:3.3.3")
-    compile ("org.apache.cxf:cxf-rt-ws-security:3.3.3")
-    compile ("org.apache.cxf:cxf-rt-ws-policy:3.3.3")
-    compile ("no.nav.security:oidc-spring-support:0.2.4")
-    compile ("no.nav.security:oidc-support:0.2.4")
-    compile ("com.microsoft.azure:adal4j:1.6.4")
-    compile ("com.nimbusds:oauth2-oidc-sdk:6.5")
-    compile ("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
-    compile ("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.2.71")
-    compile ("org.jetbrains.kotlin:kotlin-reflect:1.2.71")
-    compile ("javax.inject:javax.inject:1")
-    compile ("org.springframework.boot:spring-boot-starter-jersey:2.0.6.RELEASE")
-    compile ("org.springframework.boot:spring-boot-starter-web:2.0.6.RELEASE")
-    compile ("org.springframework.boot:spring-boot-starter-actuator:2.0.6.RELEASE")
-    compile ("org.bitbucket.b_c:jose4j:0.5.0")
-    compile ("io.micrometer:micrometer-registry-prometheus:1.0.7")
-    compile ("org.slf4j:slf4j-api:1.7.25")
-    compile ("org.springframework.boot:spring-boot-starter-logging:2.0.6.RELEASE")
-    compile ("net.logstash.logback:logstash-logback-encoder:4.10")
-    compile ("org.springframework.boot:spring-boot-starter-jta-atomikos:2.0.6.RELEASE")
-    compile ("com.oracle:ojdbc8:12.2.0.1")
-    testCompile ("no.nav.security:oidc-spring-test:0.2.4")
-    testCompile ("com.h2database:h2:1.4.196")
-    testCompile ("org.springframework.boot:spring-boot-starter-test:2.0.6.RELEASE")
-    compile ("org.flywaydb:flyway-core:5.0.7")
+    compile("org.apache.cxf:cxf-spring-boot-starter-jaxws:3.3.3")
+    compile("org.apache.cxf:cxf-rt-features-logging:3.3.3")
+    compile("org.apache.cxf:cxf-rt-ws-security:3.3.3")
+    compile("org.apache.cxf:cxf-rt-ws-policy:3.3.3")
+    compile("no.nav.security:oidc-spring-support:0.2.4")
+    compile("no.nav.security:oidc-support:0.2.4")
+    compile("com.microsoft.azure:adal4j:1.6.4")
+    compile("com.nimbusds:oauth2-oidc-sdk:6.5")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.2.71")
+    compile("org.jetbrains.kotlin:kotlin-reflect:1.2.71")
+    compile("javax.inject:javax.inject:1")
+    compile("org.springframework.boot:spring-boot-starter-jersey:2.0.6.RELEASE")
+    compile("org.springframework.boot:spring-boot-starter-web:2.0.6.RELEASE")
+    compile("org.springframework.boot:spring-boot-starter-actuator:2.0.6.RELEASE")
+    compile("org.bitbucket.b_c:jose4j:0.5.0")
+    compile("io.micrometer:micrometer-registry-prometheus:1.0.7")
+    compile("org.slf4j:slf4j-api:1.7.25")
+    compile("org.springframework.boot:spring-boot-starter-logging:2.0.6.RELEASE")
+    compile("net.logstash.logback:logstash-logback-encoder:4.10")
+    compile("org.springframework.boot:spring-boot-starter-jta-atomikos:2.0.6.RELEASE")
+    compile("com.oracle:ojdbc8:12.2.0.1")
+    testCompile("no.nav.security:oidc-spring-test:0.2.4")
+    testCompile("com.h2database:h2:1.4.196")
+    testCompile("org.springframework.boot:spring-boot-starter-test:2.0.6.RELEASE")
+    compile("org.flywaydb:flyway-core:5.0.7")
 }
 
 
@@ -71,6 +71,12 @@ tasks {
     create("printVersion") {
         doLast {
             println(project.version)
+        }
+    }
+
+    test {
+        doFirst {
+            systemProperty("spring.profiles.active", "test")
         }
     }
 
