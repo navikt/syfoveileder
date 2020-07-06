ca90d067df1677bae9de94701ca8f507d5b1bfae
Mathias Rørvik <mathias.fris.rorvik@nav.no>
2020-07-06 16:11:15 +0200

    fix package thingy

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,6 @@ repositories {
     mavenCentral()
     jcenter()
     maven(url = "https://dl.bintray.com/kotlin/ktor")
-    maven(url = "https://repo.adeo.no/repository/maven-releases/")
     maven(url = "https://dl.bintray.com/spekframework/spek-dev")
     maven(url = "https://dl.bintray.com/kotlin/kotlinx/")
 }
