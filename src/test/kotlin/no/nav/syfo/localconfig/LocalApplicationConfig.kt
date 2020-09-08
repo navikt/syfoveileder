12be7c96b3e1173f8d573a256aa7ea2f8ca0ef9c
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-09-08 14:01:44 +0200

    Remove unused prop

--- src/test/kotlin/no/nav/syfo/localconfig/LocalApplicationConfig.kt
+++ src/test/kotlin/no/nav/syfo/localconfig/LocalApplicationConfig.kt
@@ -3,11 +3,7 @@ package no.nav.syfo.localconfig
 import no.nav.security.oidc.test.support.spring.TokenGeneratorConfiguration
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import org.springframework.core.env.Environment
 
 @Configuration
 @Import(TokenGeneratorConfiguration::class)
-class LocalApplicationConfig(environment: Environment)/*
-            Her kan du ta inn properties som normalt settes av platformen slik at de er tilgjengelige runtime lokalt
-            Eks: System.setProperty("syfoveileder_USERNAME", environment.getProperty("syfoveileder.username"));
-         */
+class LocalApplicationConfig
