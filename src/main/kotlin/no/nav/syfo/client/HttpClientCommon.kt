075bd0d0c9d43551e207a11141cb3b2f3fea5cf8
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2022-04-21 09:55:04 +0200

    httpclient expects success (#39)

--- src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
+++ src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
@@ -2,20 +2,11 @@ package no.nav.syfo.client
 
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
-import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.jackson.*
 import no.nav.syfo.util.configure
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner
 import java.net.ProxySelector
-
-fun httpClientDefault() = HttpClient(CIO) {
-    install(ContentNegotiation) {
-        jackson {
-            configure()
-        }
-    }
-}
 
 val proxyConfig: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
     install(ContentNegotiation) {
@@ -23,6 +14,7 @@ val proxyConfig: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
             configure()
         }
     }
+    expectSuccess = true
     engine {
         customizeClient {
             setRoutePlanner(SystemDefaultRoutePlanner(ProxySelector.getDefault()))
