beca9ced6b9ecd444211ea1265aff202940dc70f
geir-waagboe <geir.arne.waagbo@nav.no>
2022-08-11 11:36:14 +0200

    Bump ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,13 @@ version = "1.0.0-SNAPSHOT"
 
 object Versions {
     const val jackson = "2.13.3"
-    const val ktor = "2.0.2"
+    const val ktor = "2.0.3"
     const val kluent = "1.68"
     const val logback = "1.2.11"
     const val logstashEncoder = "7.2"
     const val mockk = "1.12.4"
     const val nimbusJoseJwt = "9.23"
-    const val micrometerRegistry = "1.9.1"
+    const val micrometerRegistry = "1.9.2"
     const val spek = "2.0.18"
 }
 
