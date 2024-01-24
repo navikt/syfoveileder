11eb6345f213855fd76ef15ad5e18ce29023f1b1
geir-waagboe <geir.arne.waagbo@nav.no>
2024-01-24 12:45:53 +0100

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,21 +4,22 @@ group = "no.nav.syfo"
 version = "1.0.0-SNAPSHOT"
 
 object Versions {
-    const val jacksonDataType = "2.15.2"
-    const val jedis = "4.4.3"
-    const val ktor = "2.3.2"
+    const val jacksonDataType = "2.16.0"
+    const val jedis = "5.1.0"
+    const val json = "20231013"
+    const val ktor = "2.3.7"
     const val kluent = "1.73"
-    const val logback = "1.4.7"
-    const val logstashEncoder = "7.3"
-    const val mockk = "1.13.5"
-    const val nimbusJoseJwt = "9.31"
-    const val micrometerRegistry = "1.11.1"
+    const val logback = "1.4.14"
+    const val logstashEncoder = "7.4"
+    const val mockk = "1.13.8"
+    const val nimbusJoseJwt = "9.37.2"
+    const val micrometerRegistry = "1.12.0"
     const val redisEmbedded = "0.7.3"
     const val spek = "2.0.19"
 }
 
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.22"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.jlleitschuh.gradle.ktlint") version "11.4.2"
 }
@@ -44,6 +45,7 @@ dependencies {
     // Logging
     implementation("ch.qos.logback:logback-classic:${Versions.logback}")
     implementation("net.logstash.logback:logstash-logback-encoder:${Versions.logstashEncoder}")
+    implementation("org.json:json:${Versions.json}")
 
     // Metrics and Prometheus
     implementation("io.ktor:ktor-server-metrics-micrometer:${Versions.ktor}")
